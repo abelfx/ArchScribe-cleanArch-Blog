0892d9d6e133d67a9f7b66cae4ca8ca5a436dd63
Abel Tesfa <abeltesfa198@gmail.com>
2025-08-04 15:17:29 +0300

    Basic CRUD for blog operations done

--- usecases/blog_usecase.go
+++ usecases/blog_usecase.go
@@ -1,1 +1,43 @@
 package usecases
+
+import (
+	"Blog/domain"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// struct to implement usecase
+type TaskUsecase struct {
+	repo domain.BlogRepository
+}
+
+// constructor for TaskUsecase
+func NewTaskUsecase(repo domain.BlogRepository) *TaskUsecase {
+	return &TaskUsecase{repo: repo}
+}
+
+
+// create blog
+func(u *TaskUsecase) CreateBlog(blog *domain.Blog) (*domain.Blog, error) {
+	return u.repo.CreateBlog(blog)
+}
+
+// retrive a blog using its id
+func (u *TaskUsecase) GetBlogByID(id primitive.ObjectID) (*domain.Blog, error) {
+	return u.repo.GetBlogByID(id)
+}
+
+// retrive all blogs
+func (u *TaskUsecase) GetAllBlogs() ([]*domain.Blog, error){
+	return u.repo.GetAllBlogs()
+}
+
+// update a blog using its id
+func ( u *TaskUsecase) UpdateBlog(id primitive.ObjectID, blog *domain.Blog) (*domain.Blog, error) {
+	return u.repo.UpdateBlog(id, blog)
+}
+
+// delete a blog using its id
+func ( u *TaskUsecase) DeleteBlog(id primitive.ObjectID) error {
+	return u.repo.DeleteBlog(id)
+}
